535878c1f1a35b93a7d0c847252841695f4c7d3f
dreamersole <dreamersole@users.noreply.github.com>
2020-09-15 19:48:51 +0300

    Task 8 - Main Page - Product Stickers Test

---
+++ src/test/java/org/example/StickerTest.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleContains;
+
+public class StickerTest {
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new FirefoxDriver();
+        //wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void stickerTest() {
+        // Open main page
+        driver.get("http://localhost/litecart/");
+        // Find products shown on the main page
+        List<WebElement> product_list = driver.findElements(By.cssSelector("li.product"));
+        // Check for sticker
+        for (int i = 0, n = product_list.size(); i < n; ++i) {
+            assert product_list.get(i).findElements(By.cssSelector("div.sticker")).size() == 1;
+        }
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
