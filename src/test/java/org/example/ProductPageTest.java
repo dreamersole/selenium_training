6218bb5049b2461f28e66fd3564c4753c56c36fd
dreamersole <dreamersole@users.noreply.github.com>
2020-09-18 18:08:31 +0300

    Task 10 - code cleanup, note:
    bold/strike-out are not checked itself, but through tag/class names only

--- src/test/java/org/example/ProductPageTest.java
+++ src/test/java/org/example/ProductPageTest.java
@@ -37,33 +37,45 @@ public class ProductPageTest {
     }
 
     private void productPageTestInternal() {
+        // MAIN PAGE
         openMainPage();
         // Get the campaigns first found product
         WebElement product = driver.findElement(By.cssSelector("div#box-campaigns li.product"));
         // Read the product info from the main page:
         String main_product_name = product.findElement(By.cssSelector("div.name")).getText();
-        String main_regular_price = product.findElement(By.cssSelector("s.regular-price")).getText();
-        String main_campaign_price = product.findElement(By.cssSelector("strong.campaign-price")).getText();
-        String main_regular_price_font_color = product.findElement(By.cssSelector("s.regular-price")).getCssValue("color");
-        String main_campaign_price_font_color = product.findElement(By.cssSelector("strong.campaign-price")).getCssValue("color");
-        // - normal_cost_font_is_crossed
-        // - action_cost_font_is_bold
-        Dimension main_regular_price_font_size = product.findElement(By.cssSelector("s.regular-price")).getSize();
-        Dimension main_campaign_price_font_size = product.findElement(By.cssSelector("strong.campaign-price")).getSize();
-        // TODO
+        WebElement main_regular_price_web_element = product.findElement(By.cssSelector("s.regular-price"));
+        WebElement main_campaign_price_web_element = product.findElement(By.cssSelector("strong.campaign-price"));
+        String main_regular_price = main_regular_price_web_element.getText();
+        String main_campaign_price = main_campaign_price_web_element.getText();
+        // NOTE: for the main page prices
+        // font color, size and style (bold, line-through) is defined by css or by default for the elements:
+        // s.regular-price-> text-decoration-line: line-through, color: #777, font-size: 0.8em*18px
+        // strong.campaign-price-> font-weight: bold, color: #c00, font-size: 18px
+        // So, we may check tag and class names only, but we can also check some properties automatically:
+        String main_regular_price_font_color = main_regular_price_web_element.getCssValue("color");
+        String main_campaign_price_font_color = main_campaign_price_web_element.getCssValue("color");
+        int main_regular_price_text_height = main_regular_price_web_element.getSize().height; // not font itself, but text placeholder height
+        int main_campaign_price_text_height = main_campaign_price_web_element.getSize().height; // not font itself, but text placeholder height
+
+        // PRODUCT PAGE
         // Open the campaigns first found product page
         product.click();
         // Read the product info from the product page:
         String prod_product_name = driver.findElement(By.cssSelector("div#box-product h1.title")).getText();
-        String prod_regular_price = driver.findElement(By.cssSelector("div#box-product s.regular-price")).getText();
-        String prod_campaign_price = driver.findElement(By.cssSelector("div#box-product strong.campaign-price")).getText();
-        String prod_regular_price_font_color = driver.findElement(By.cssSelector("div#box-product s.regular-price")).getCssValue("color");
-        String prod_campaign_price_font_color = driver.findElement(By.cssSelector("div#box-product strong.campaign-price")).getCssValue("color");
-        // - normal_cost_font_is_crossed
-        // - action_cost_font_is_bold
-        Dimension prod_regular_price_font_size = driver.findElement(By.cssSelector("div#box-product s.regular-price")).getSize();
-        Dimension prod_campaign_price_font_size = driver.findElement(By.cssSelector("div#box-product strong.campaign-price")).getSize();
-        // TODO
+        WebElement prod_regular_price_web_element = product.findElement(By.cssSelector("div#box-product .price-wrapper s.regular-price"));
+        WebElement prod_campaign_price_web_element = product.findElement(By.cssSelector("div#box-product .price-wrapper strong.campaign-price"));
+        String prod_regular_price = prod_regular_price_web_element.getText();
+        String prod_campaign_price = prod_campaign_price_web_element.getText();
+        // NOTE: for the product page prices
+        // font color, size and style (bold, line-through) is defined by css or by default for the elements:
+        // div#box-product .price-wrapper s.regular-price-> text-decoration-line: line-through, color: #666, font-size: 16px
+        // div#box-product .price-wrapper strong.campaign-price-> font-weight: bold, color: #c00, font-size: 22px
+        // So, we may check tag and class names only, but we can also check some properties automatically:
+        String prod_regular_price_font_color = prod_regular_price_web_element.getCssValue("color");
+        String prod_campaign_price_font_color = prod_campaign_price_web_element.getCssValue("color");
+        int prod_regular_price_text_height = prod_regular_price_web_element.getSize().height; // not font itself, but text placeholder height
+        int prod_campaign_price_text_height = prod_campaign_price_web_element.getSize().height; // not font itself, but text placeholder height
+
         // Verify all info
         assert main_product_name.equals(prod_product_name);
         assert main_campaign_price.equals(prod_campaign_price);
@@ -72,9 +84,9 @@ public class ProductPageTest {
         assert isGrey(main_regular_price_font_color);
         assert isRed(prod_campaign_price_font_color);
         assert isGrey(prod_regular_price_font_color);
-        // TODO
-        assert main_campaign_price_font_size.height > main_regular_price_font_size.height;
-        assert prod_campaign_price_font_size.height > prod_regular_price_font_size.height;
+        assert main_campaign_price_text_height > main_regular_price_text_height;
+        assert prod_campaign_price_text_height > prod_regular_price_text_height;
+        // NOTE: other font style parameters (bold, line-through) are defined in css for the tags/classes and checked implicitly/manually
     }
 
     private void openMainPage() {
@@ -87,7 +99,7 @@ public class ProductPageTest {
         int r = c.getRed();
         int g = c.getGreen();
         int b = c.getBlue();
-        int a = c.getAlpha();
+        //int a = c.getAlpha();
         return r > 0 && g == 0 && b == 0;
     }
 
@@ -97,7 +109,7 @@ public class ProductPageTest {
         int r = c.getRed();
         int g = c.getGreen();
         int b = c.getBlue();
-        int a = c.getAlpha();
+        //int a = c.getAlpha();
         return r == g && g == b && b > 0;
     }
 
