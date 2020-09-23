a4d9eb4789ae5b518bb7ef79d0c639d9e64a0a02
dreamersole <dreamersole@users.noreply.github.com>
2020-09-23 17:31:12 +0300

    Task 11 - intermediate commit

--- src/test/java/org/example/UserAddTest.java
+++ src/test/java/org/example/UserAddTest.java
@@ -8,7 +8,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.Color;
+
+import java.security.SecureRandom;
 //import org.openqa.selenium.support.ui.WebDriverWait;
 
 
@@ -36,89 +37,58 @@ public class UserAddTest {
     }
 
     private void testInternal() {
-        // MAIN PAGE
+        // Open main page
         openMainPage();
-        // Get the campaigns first found product
-        WebElement product = driver.findElement(By.cssSelector("div#box-campaigns li.product"));
-        // Read the product info from the main page:
-        String main_product_name = product.findElement(By.cssSelector("div.name")).getText();
-        WebElement main_regular_price_web_element = product.findElement(By.cssSelector("s.regular-price"));
-        WebElement main_campaign_price_web_element = product.findElement(By.cssSelector("strong.campaign-price"));
-        String main_regular_price = main_regular_price_web_element.getText();
-        String main_campaign_price = main_campaign_price_web_element.getText();
-        // NOTE: for the main page prices
-        // font color, size and style (bold, line-through) is defined by css or by default for the elements:
-        // s.regular-price-> text-decoration-line: line-through, color: #777, font-size: 0.8em*18px
-        // strong.campaign-price-> font-weight: bold, color: #c00, font-size: 18px
-        // So, we may check tag and class names only, but we can also check some properties automatically:
-        String main_regular_price_font_color = main_regular_price_web_element.getCssValue("color");
-        String main_campaign_price_font_color = main_campaign_price_web_element.getCssValue("color");
-        String main_regular_price_font_size = main_regular_price_web_element.getCssValue("font-size");
-        String main_campaign_price_font_size = main_campaign_price_web_element.getCssValue("font-size");
-
-        // PRODUCT PAGE
-        // Open the campaigns first found product page
-        product.click();
-        // Read the product info from the product page:
-        String prod_product_name = driver.findElement(By.cssSelector("div#box-product h1.title")).getText();
-        WebElement prod_regular_price_web_element = driver.findElement(By.cssSelector("div#box-product .price-wrapper s.regular-price"));
-        WebElement prod_campaign_price_web_element = driver.findElement(By.cssSelector("div#box-product .price-wrapper strong.campaign-price"));
-        String prod_regular_price = prod_regular_price_web_element.getText();
-        String prod_campaign_price = prod_campaign_price_web_element.getText();
-        // NOTE: for the product page prices
-        // font color, size and style (bold, line-through) is defined by css or by default for the elements:
-        // div#box-product .price-wrapper s.regular-price-> text-decoration-line: line-through, color: #666, font-size: 16px
-        // div#box-product .price-wrapper strong.campaign-price-> font-weight: bold, color: #c00, font-size: 22px
-        // So, we may check tag and class names only, but we can also check some properties automatically:
-        String prod_regular_price_font_color = prod_regular_price_web_element.getCssValue("color");
-        String prod_campaign_price_font_color = prod_campaign_price_web_element.getCssValue("color");
-        String prod_regular_price_font_size = prod_regular_price_web_element.getCssValue("font-size");
-        String prod_campaign_price_font_size = prod_campaign_price_web_element.getCssValue("font-size");
-
-        // Verify all info
-        assert main_product_name.equals(prod_product_name);
-        assert main_campaign_price.equals(prod_campaign_price);
-        assert main_regular_price.equals(prod_regular_price);
-        assert isRed(main_campaign_price_font_color);
-        assert isGrey(main_regular_price_font_color);
-        assert isRed(prod_campaign_price_font_color);
-        assert isGrey(prod_regular_price_font_color);
-        assert parseFontSize(main_campaign_price_font_size) > parseFontSize(main_regular_price_font_size);
-        assert parseFontSize(prod_campaign_price_font_size) > parseFontSize(prod_regular_price_font_size);
-        // NOTE: other font style parameters (bold, line-through) are defined in css for the tags/classes and checked implicitly/manually
+        // Initiate user account creating
+        driver.findElement(By.xpath("//a[contains(@href, 'create_account')]")).click();
+        // Fill user account form
+        fillFieldByName("firstname", "Testfirstname");
+        fillFieldByName("lastname", "Testlastname");
+        fillFieldByName("address1", "Test Address 1");
+        fillFieldByName("postcode", "12345");
+        fillFieldByName("city", "Test City");
+        // TODO: Country
+        // TODO: State
+        fillFieldByName("email", getRandomEmail());
+        fillFieldByName("phone", "+1234567890");
+        fillFieldByName("password", "secret");
+        fillFieldByName("confirmed_password", "secret");
+        // TODO
+        // Submit (and implicit login)
+        // TODO
+        // Logout
+        // TODO
+        // Login #2
+        // TODO
+        // Logout #2
     }
 
     private void openMainPage() {
         driver.get("http://localhost/litecart/");
     }
 
-    boolean isRed(String color_string) {
-        Color color = Color.fromString(color_string);
-        java.awt.Color c = color.getColor();
-        int r = c.getRed();
-        int g = c.getGreen();
-        int b = c.getBlue();
-        //int a = c.getAlpha();
-        return r > 0 && g == 0 && b == 0;
+    private void fillFieldByName(String name, String value) {
+        driver.findElement(By.name(name)).sendKeys(value);
     }
 
-    boolean isGrey(String color_string) {
-        Color color = Color.fromString(color_string);
-        java.awt.Color c = color.getColor();
-        int r = c.getRed();
-        int g = c.getGreen();
-        int b = c.getBlue();
-        //int a = c.getAlpha();
-        return r == g && g == b && b > 0;
-    }
+    static SecureRandom random = new SecureRandom();
+    static final String charsA = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    static final String charsB = charsA + ".-_";
 
-    double parseFontSize(String css_font_size) {
-        String suffix_px = "px";
-        if(css_font_size.endsWith(suffix_px)) {
-            return Double.valueOf(css_font_size.substring(0, css_font_size.length() - suffix_px.length()));
+    private String getRandomEmail() {
+        int length1 = 1 + random.nextInt(20);
+        int length2 = 1 + random.nextInt(15);
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(charsA.charAt(random.nextInt(charsA.length())));
+        for(int i = 1; i < length1; ++i) {
+            stringBuilder.append(charsB.charAt(random.nextInt(charsB.length())));
         }
-
-        throw new RuntimeException("font-size parsing error");
+        stringBuilder.append("@");
+        stringBuilder.append(charsA.charAt(random.nextInt(charsA.length())));
+        for(int i = 1; i < length2; ++i) {
+            stringBuilder.append(charsB.charAt(random.nextInt(charsB.length())));
+        }
+        return stringBuilder.toString();
     }
 
     @After
